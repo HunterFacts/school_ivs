3ecfea89d686fd4ae045e93e6d5983b701faf532
Lukanin Mikhail <mlukanin@ics.perm.ru>
2019-04-03 13:10:24 +0500

    Почти допилил первый модуль

--- module1_restapi/js/main.js
+++ module1_restapi/js/main.js
@@ -1,6 +1,17 @@
 var ch = 1;
+var idValue = 0;
 $(document).ready(function(){
-    $('.modal').modal();
+    $('.modal').modal({
+      onCloseEnd: function(){
+          $('#number').val("");
+          $('#supply_date').val("");
+          $('#comment').val("");
+          $('#invoice_date').val("");
+          $("#checker").prop("checked", false);
+          $("#addBtn").html("Добавить");
+          idValue = 0;
+      }
+    });
     $('.datepicker').datepicker({
       format: 'dd-mm-yyyy',
       i18n: {
@@ -27,18 +38,18 @@ $(document).ready(function(){
         ch == 1;
       }
     }, 30000);
-    $.getJSON('http://localhost:3000/inv?_limit=5',
+    $.getJSON('http://localhost:3000/inv?_limit=10',
     function(data) {
       $.each(data, function(key,value) {
         $('#table-main tbody').append(
-          '<tr>'
+          '<tr id="data_'+value.id+'">'
               +'<td>'+value.create+'</td>'
               +'<td>'+value.number+'</td>'
               +'<td>'+value.supply+'</td>'
               +'<td>'+value.comment+'</td>'
             +'<td style="width: 300px;">'
-              +'<a class="waves-effect waves-light btn" data_id="'+key+'"><i class="material-icons left">create</i>Изменить '+key+'</a>'
-              +'<a class="waves-effect waves-light btn" data_id="'+key+'"><i class="material-icons left">clear</i>Удалить</a>'
+              +'<a class="waves-effect waves-light btn modal-trigger" href="#modal-create" onclick="Edit ('+value.id+');" data_id="'+value.id+'"><i class="material-icons left">create</i>Изменить</a>'
+              +'<a class="waves-effect waves-light btn" onclick="Delete ('+value.id+');" data_id="'+value.id+'"><i class="material-icons left">clear</i>Удалить</a>'
             +'</td>'
           +'</tr>'
         )
@@ -48,18 +59,18 @@ $(document).ready(function(){
 });
 
 function EveryBtn (id){
-  $.getJSON('http://localhost:3000/inv?_limit=5&_page='+id,
+  $.getJSON('http://localhost:3000/inv?_limit=10&_page='+id,
   function(data) {
     $.each(data, function(key,value) {
       $('#table-main tbody').append(
-        '<tr>'
+        '<tr id="data_'+value.id+'">'
             +'<td>'+value.create+'</td>'
             +'<td>'+value.number+'</td>'
             +'<td>'+value.supply+'</td>'
             +'<td>'+value.comment+'</td>'
           +'<td style="width: 300px;">'
-            +'<a class="waves-effect waves-light btn" data_id="'+value.id+'"><i class="material-icons left">create</i>Изменить '+value.id+'</a>'
-            +'<a class="waves-effect waves-light btn" data_id="'+value.id+'"><i class="material-icons left">clear</i>Удалить</a>'
+            +'<a class="waves-effect waves-light btn modal-trigger" href="#modal-create" onclick="Edit ('+value.id+');" data_id="'+value.id+'"><i class="material-icons left">create</i>Изменить</a>'
+            +'<a class="waves-effect waves-light btn" onclick="Delete ('+value.id+');" data_id="'+value.id+'"><i class="material-icons left">clear</i>Удалить</a>'
           +'</td>'
         +'</tr>'
       )
@@ -67,5 +78,98 @@ function EveryBtn (id){
     id=id+1;
     $('#every-btn').html('<a class="waves-effect waves-light btn" onclick="EveryBtn('+id+')"><i class="material-icons left">add_circle_outline</i>Еще загрузить</a>');
   });
+}
+function htmlEscape(str) {
+  return String(str)
+          .replace(/&/g, '&amp;')
+          .replace(/"/g, '&quot;')
+          .replace(/'/g, '&#39;')
+          .replace(/</g, '&lt;')
+          .replace(/>/g, '&gt;');
+}
+function CreateString(){
+  var number = htmlEscape($('#number').val());
+  var invoice = htmlEscape($('#invoice_date').val());
+  var supply_date = htmlEscape($('#supply_date').val());
+  var comment = htmlEscape($('#comment').val());
 
+  if (number != "" || number != null){
+    if (invoice != "" || invoice != null){
+      if (supply_date != "" || supply_date != null){
+        if (comment != "" || comment != null){
+          if ($("#checker").prop("checked")){
+            $.ajax({
+              url: "http://localhost:3000/inv/" + idValue,
+              type: "PATCH",
+              data:
+              {
+                "create": invoice,
+                "number": number,
+                "supply": supply_date,
+                "comment": comment
+              },
+
+            });
+            $("#data_"+idValue+" td:nth-child(2)").html($('#number').val());
+            $("#data_"+idValue+" td:nth-child(1)").html($('#invoice_date').val());
+            $("#data_"+idValue+" td:nth-child(3)").html($('#supply_date').val());
+            $("#data_"+idValue+" td:nth-child(4)").html($('#comment').val());
+            idValue = 0;
+            $('#number').val("");
+            $('#supply_date').val("");
+            $('#comment').val("");
+            $('#invoice_date').val("");
+            $("#checker").prop("checked", false);
+            $("#addBtn").html("Добавить");
+          }
+          else {
+            $.post('http://localhost:3000/inv',
+                {
+                  "create": invoice,
+                  "number": number,
+                  "supply": supply_date,
+                  "comment": comment
+                }
+            )
+          }
+        }
+        else {
+          M.toast({html: 'Введите комментарий'});
+        }
+      }
+      else {
+        M.toast({html: 'Введите дату'});
+      }
+    }
+    else {
+      M.toast({html: 'Введите дату'});
+    }
+  }
+  else {
+    M.toast({html: 'Введите номер'});
+  }
+}
+
+function Edit (editId){
+  if (editId != ""){
+    idValue = editId;
+    $('#number-l').addClass('active');
+    $('#invoice_date-l').addClass('active');
+    $('#supply_date-l').addClass('active');
+    $('#comment-l').addClass('active');
+    $('#number').val($("#data_"+editId+" td:nth-child(2)").text());
+    $('#invoice_date').val($("#data_"+editId+" td:nth-child(1)").text());
+    $('#supply_date').val($("#data_"+editId+" td:nth-child(3)").text());
+    $('#comment').val($("#data_"+editId+" td:nth-child(4)").text());
+    $("#addBtn").html("Изменить");
+    $("#checker").prop("checked", true);
+  }
+}
+
+function Delete (deleteId){
+  $.ajax({
+    url: "http://localhost:3000/inv/" + deleteId,
+    type: "DELETE"
+  });
+  $("#data_"+deleteId).hide();
 }
